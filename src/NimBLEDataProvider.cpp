5ec6ceb2e457e7872b345ccf760d10c4c20a2a18
Jonas Stolle <jonas.stolle@sensirion.com>
2022-01-10 16:12:32 +0100

    Add NimBLEDataProvider skeleton as IDataProvider interface realization

---
+++ src/NimBLEDataProvider.cpp
@@ -0,0 +1,14 @@
+#include "NimBLEDataProvider.h"
+#include <NimBLEDevice.h>
+
+void NimBLEDataProvider::begin() {
+}
+
+void NimBLEDataProvider::writeValue(float value) {
+}
+
+void NimBLEDataProvider::commit() {
+}
+
+void NimBLEDataProvider::handleEvents() {
+}
