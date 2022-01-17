07db2a21112afa073b03f3efa5e5493c4c0899fa
Jonas Stolle <jonas.stolle@sensirion.com>
2022-01-17 16:04:11 +0100

    Add example for testing of refactored code

--- examples/Example_Refactor_Testing/Example_Refactor_Testing.ino
+++ examples/Example_Refactor_Testing/Example_Refactor_Testing.ino
@@ -1,9 +1,19 @@
-#include "NimBLEDataProvider.h"
+#include "DataProvider.h"
+#include "NimBLELibraryWrapper.h"
+
+NimBLELibraryWrapper lib;
+DataProvider provider(lib);
+
+float mockTemp = 0;
+float mockHumi = 0;
 
 void setup(){
-
+    Serial.begin(115200);
+    provider.begin();
 }
 
 void loop(){
-    
+    delay(100);
+    provider.writeValue(++mockTemp, Unit::T);
+    provider.commit();
 }
