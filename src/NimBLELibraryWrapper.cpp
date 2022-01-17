0a7a02bd523a1ff55505c35871440964ef9337d1
Jonas Stolle <jonas.stolle@sensirion.com>
2022-01-17 10:41:15 +0100

    Implement library wrapper interface for NimBLE

---
+++ src/NimBLELibraryWrapper.cpp
@@ -0,0 +1,31 @@
+#include "NimBLELibraryWrapper.h"
+
+void NimBLELibraryWrapper::initDevice(const std::string& deviceName) {
+    NimBLEDevice::init(deviceName);
+    _deviceName = deviceName;
+}
+
+void NimBLELibraryWrapper::initAdvertising() {
+    _pNimBLEAdvertising = NimBLEDevice::getAdvertising();
+    // Helps with iPhone connection issues (copy/paste)
+    _pNimBLEAdvertising->setMinPreferred(0x06);
+    _pNimBLEAdvertising->setMaxPreferred(0x12);
+}
+
+void NimBLELibraryWrapper::setAdvertisingData(const std::string& data) {
+    NimBLEAdvertisementData advert;
+    advert.setName(_deviceName);
+    advert.setManufacturerData(data);
+    _pNimBLEAdvertising->setAdvertisementData(advert);
+}
+
+void NimBLELibraryWrapper::startAdvertising() {
+    _pNimBLEAdvertising->start();
+}
+
+void NimBLELibraryWrapper::stopAdvertising() {
+    _pNimBLEAdvertising->stop();
+}
+std::string NimBLELibraryWrapper::getDeviceAddress() {
+    return NimBLEDevice::getAddress().toString();
+}
