ad9a21408390b79d974a702c53ddcdd881ac9ac5
Jonas Stolle <jonas.stolle@sensirion.com>
2022-01-25 09:01:13 +0100

    Drop class qualifiers of static member _data

--- src/NimBLELibraryWrapper.cpp
+++ src/NimBLELibraryWrapper.cpp
@@ -12,14 +12,14 @@ WrapperPrivateData* NimBLELibraryWrapper::_data = nullptr;
 
 NimBLELibraryWrapper::NimBLELibraryWrapper() {
     if (NimBLELibraryWrapper::_numberOfInstances == 0) {
-        NimBLELibraryWrapper::_data = new WrapperPrivateData();
+        _data = new WrapperPrivateData();
         ++NimBLELibraryWrapper::_numberOfInstances;
     }
 }
 
 NimBLELibraryWrapper::~NimBLELibraryWrapper() {
     if (NimBLELibraryWrapper::_numberOfInstances == 1) {
-        delete NimBLELibraryWrapper::_data;
+        delete _data;
         _deinit();
         --NimBLELibraryWrapper::_numberOfInstances;
     }
@@ -28,7 +28,7 @@ NimBLELibraryWrapper::~NimBLELibraryWrapper() {
 void NimBLELibraryWrapper::_deinit() {
     if (_data->BLEDeviceRunning) {
         NimBLEDevice::deinit(true);
-        NimBLELibraryWrapper::_data->BLEDeviceRunning = false;
+        _data->BLEDeviceRunning = false;
     }
 }
 
@@ -39,27 +39,25 @@ void NimBLELibraryWrapper::init() {
     NimBLEDevice::init(GADGET_NAME);
     _data->BLEDeviceRunning = true;
 
-    NimBLELibraryWrapper::_data->_pNimBLEAdvertising =
-        NimBLEDevice::getAdvertising();
+    _data->_pNimBLEAdvertising = NimBLEDevice::getAdvertising();
     // Helps with iPhone connection issues (copy/paste)
-    NimBLELibraryWrapper::_data->_pNimBLEAdvertising->setMinPreferred(0x06);
-    NimBLELibraryWrapper::_data->_pNimBLEAdvertising->setMaxPreferred(0x12);
+    _data->_pNimBLEAdvertising->setMinPreferred(0x06);
+    _data->_pNimBLEAdvertising->setMaxPreferred(0x12);
 }
 
 void NimBLELibraryWrapper::setAdvertisingData(const std::string& data) {
     NimBLEAdvertisementData advert;
     advert.setName(GADGET_NAME);
     advert.setManufacturerData(data);
-    NimBLELibraryWrapper::_data->_pNimBLEAdvertising->setAdvertisementData(
-        advert);
+    _data->_pNimBLEAdvertising->setAdvertisementData(advert);
 }
 
 void NimBLELibraryWrapper::startAdvertising() {
-    NimBLELibraryWrapper::_data->_pNimBLEAdvertising->start();
+    _data->_pNimBLEAdvertising->start();
 }
 
 void NimBLELibraryWrapper::stopAdvertising() {
-    NimBLELibraryWrapper::_data->_pNimBLEAdvertising->stop();
+    _data->_pNimBLEAdvertising->stop();
 }
 std::string NimBLELibraryWrapper::getDeviceAddress() {
     return NimBLEDevice::getAddress().toString();
