bd4ca3878888032a621a608df8f6c918ddf555f8
Jonas Stolle <jonas.stolle@sensirion.com>
2022-03-11 16:05:17 +0100

    Remove Serial output from library

--- src/WifiCredentialSetters.cpp
+++ src/WifiCredentialSetters.cpp
@@ -1,15 +1,12 @@
 #include "DataProvider.h"
 
 void DataProvider::onWifiSsidChange(std::string ssid) {
-    Serial.print("Ssid changed to ");
-    Serial.println(ssid.c_str());
     if (_pWifiLibaray != nullptr) {
         _pWifiLibaray->setSsid(ssid);
     }
 }
 
 void DataProvider::onWifiPasswordChange(std::string pwd) {
-    Serial.println("Connecting to wifi using given password");
     if (_pWifiLibaray != nullptr) {
         _pWifiLibaray->connect(pwd);
     }
