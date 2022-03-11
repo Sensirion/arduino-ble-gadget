d11d522f07150638cd408ff11244811077f5226a
Jonas Stolle <jonas.stolle@sensirion.com>
2022-03-11 15:38:28 +0100

    Add wifi interface as optional DataProvider Member

--- src/WifiCredentialSetters.cpp
+++ src/WifiCredentialSetters.cpp
@@ -1,14 +1,16 @@
 #include "DataProvider.h"
 
 void DataProvider::onWifiSsidChange(std::string ssid) {
-    Serial.print("Ssid changed to "); // mock
+    Serial.print("Ssid changed to ");
     Serial.println(ssid.c_str());
-    // Call relevant methods of wifi library of choice to change ssid
+    if (_pWifiLibaray != nullptr) {
+        _pWifiLibaray->setSsid(ssid);
+    }
 }
 
 void DataProvider::onWifiPasswordChange(std::string pwd) {
-    Serial.println("Password Changed!"); // mock
-    // Call relevant methods of wifi library of choice to handle password
-    // input. Best not to save it but to directly use the pwd argument
-    // to establish a connection.
+    Serial.println("Connecting to wifi using given password");
+    if (_pWifiLibaray != nullptr) {
+        _pWifiLibaray->connect(pwd);
+    }
 }
