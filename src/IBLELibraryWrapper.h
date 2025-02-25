ea9332e9c380601c97dd3b31c0fc48d8ab89350c
Heiko-L <606794+Heiko-L@users.noreply.github.com>
2025-02-25 16:36:12 +0100

    added new characteristics for SCD devices

--- src/IBLELibraryWrapper.h
+++ src/IBLELibraryWrapper.h
@@ -67,9 +67,23 @@ static const char* const SCD_SERVICE_UUID =
     "00007000-b38d-4985-720e-0f993a68ee41";
 static const char* const SCD_FRC_REQUEST_UUID =
     "00007004-b38d-4985-720e-0f993a68ee41";
+static const char* const SCD_REQUEST_UUID_MEASUREMENT_INTERVAL =
+    "00007005-b38d-4985-720e-0f993a68ee41";
+static const char* const SCD_REQUEST_UUID_TEMPERATURE_OFFSET =
+    "00007006-b38d-4985-720e-0f993a68ee41";
+static const char* const SCD_REQUEST_UUID_ALTITUDE_SETTING =
+    "00007007-b38d-4985-720e-0f993a68ee41";
+static const char* const SCD_REQUEST_UUID_ASC_STATUS =
+    "00007008-b38d-4985-720e-0f993a68ee41";
+static const char* const SCD_REQUEST_UUID_ASC_INTERVAL =
+    "00007009-b38d-4985-720e-0f993a68ee41";
+static const char* const SCD_REQUEST_UUID_ASC_INITIAL_INTERVAL =
+    "00007010-b38d-4985-720e-0f993a68ee41";
+static const char* const SCD_REQUEST_UUID_ASC_TARGET =
+    "00007011-b38d-4985-720e-0f993a68ee41";
 
 static const unsigned int MAX_NUMBER_OF_SERVICES = 4;
-static const unsigned int MAX_NUMBER_OF_CHARACTERISTICS = 12;
+static const unsigned int MAX_NUMBER_OF_CHARACTERISTICS = 16;
 
 enum Permission {
     READWRITE_PERMISSION,
